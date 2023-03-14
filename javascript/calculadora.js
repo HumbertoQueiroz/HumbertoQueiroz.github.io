89d6a9764bb4d67adc14a89ded0c63dd79c8db81
Humberto Queiroz <90359980+HumbertoQueiroz@users.noreply.github.com>
2023-03-14 18:43:36 -0400

    Update calculadora.js
    Signed-off-by: Humberto Queiroz <90359980+HumbertoQueiroz@users.noreply.github.com>

--- javascript/calculadora.js
+++ javascript/calculadora.js
@@ -7,7 +7,15 @@ var y2;
 //Várialvel que vai armazenar a operação
 var operacao;
 
+var para = document.queryseletor('.display');
 
+function addNumeroDisplay(e){
+      if(para=0){
+            para=e
+      } else{
+            para = para+e;
+      };
+};
 //Primeiro vamos criar uma lista com o querySelectorAll das minhas classes que queremos adicionar o evento
 var listaNumeros1 = document.querySelectorAll('.numero');
 
@@ -15,27 +23,4 @@ var n1 = document.getElementById('#numero1');
 var n11 = parseInt(n1.value);
       console.log(n1.value);
       console.log(n11);
-
-
-//Função que vai adicionar o evento Onclick nos números.
-
-//Esta função vai receber dois parametros/elementos: "classe" que vai ser adcionado o evento e a "função" que vai ser executada quando clicar.
-function addEventoOnclickNumero(classe, funcao) {
-    
-    
-
-    //Fazer um loop dentro da minha lista "listaNumeros1" 
-    for (i = 0; i < listaNumeros1.length; i++){
-
-        //Adicionar o evento ".onclick" recebendo uma função que vai ser executada pelo onclick, esta função vai ser informada quando chamar esta função "addEventoOnclickNumero". 
-        listaNumeros1[i].onclick = funcao;
-    };
-};
-
-//Aqui estou chamando a função e passando os parametros
-addEventoOnclickNumero("numero", addNumeroDisplay);
-
-    function addNumeroDisplay(botao) {
-    n1=botao.classList;
-    console.log(botao);
 };
