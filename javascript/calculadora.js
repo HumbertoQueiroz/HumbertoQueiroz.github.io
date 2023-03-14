6d3cb377a17d49866a0524f829b43be88a4aa8e1
Humberto Queiroz <90359980+HumbertoQueiroz@users.noreply.github.com>
2023-03-14 19:29:59 -0400

    Update calculadora.js
    Signed-off-by: Humberto Queiroz <90359980+HumbertoQueiroz@users.noreply.github.com>

--- javascript/calculadora.js
+++ javascript/calculadora.js
@@ -11,12 +11,22 @@ var para = document.querySelector('.display');
 
 function addNumeroDisplay(e){
       if(para=0){
-            para=e
+            para=e;
+            console.log('Para1=',para);
       } else{
             para = para+e;
+            console.log('Para1=',para);
       };
 };
 
 function armazenaOperacao (e){
-      
+      x1=para;
+      operacao=e
 };
+
+console.log('display=',display);
+
+console.log('x1=',x1);
+console.log('y2=',y2);
+console.log('operação=',operacao);
+console.log('Parágrafo final=',para);
