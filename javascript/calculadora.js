b91208bbd455e9be8c4cf14b6dd3b097705fb69f
Humberto Queiroz <90359980+HumbertoQueiroz@users.noreply.github.com>
2023-03-15 17:34:17 -0400

    add

--- javascript/calculadora.js
+++ javascript/calculadora.js
@@ -7,34 +7,38 @@ var y2;
 //Várialvel que vai armazenar a operação
 var operacao;
 
-var $wrapper = document.querySelector('.display');
-
+var display = document.getElementById('display2');
+var para = parseInt(display.value);
+      
 function addNumeroDisplay(e){
-      if($wrapper=0){
-            HTMLTemporario=$wrapper.innerHTML;
-            HTMLNovo=e;
-            HTMLTemporario=HTMLNovo;
-            HTML.innerHTML=HTMLTemporario;
+      if (para == 0) {
+            HTMLTemporario = display.innerHTML;
+            HTMLTNovo=e;
+            HTMLTemporario=HTMLTNovo;
+            display.innerHTML = HTMLTemporario;
+            para=display.innerHTML
             
-            console.log('Para1=',$wrapper);
-            
-      } else{
-            HTMLTemporario = $wrapper.innerHTML;
+    
+      } else {      
+            HTMLTemporario = display.innerHTML;
             HTMLTNovo=e;
             HTMLTemporario=HTMLTemporario+HTMLTNovo;
-            $wrapper.innerHTML=HTMLTemporario;
-            
-            console.log('Para1=',$wrapper);
-      };
+            display.innerHTML=HTMLTemporario;
+      };      
+    
+
 };
 
 function armazenaOperacao (e){
-      x1=para;
-      operacao=e
+      x1 = display.value;
+      console.log('x1=',x1);
+      operacao = e;
+      console.log(operacao);
+      display.innerHTML = 0;
 };
 
 
 console.log('x1=',x1);
 console.log('y2=',y2);
 console.log('operação=',operacao);
-console.log('Parágrafo final=',$wrapper);
+console.log('Parágrafo final=');
