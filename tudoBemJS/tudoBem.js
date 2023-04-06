3438928ba758f24a5dd32688117b6619bc2d11ed
Humberto Queiroz <90359980+HumbertoQueiroz@users.noreply.github.com>
2023-04-05 21:28:22 -0400

    Update tudoBem.js
    Signed-off-by: Humberto Queiroz <90359980+HumbertoQueiroz@users.noreply.github.com>

--- tudoBemJS/tudoBem.js
+++ tudoBemJS/tudoBem.js
@@ -7,11 +7,11 @@ function alerta() {
     if (tudoBem) {
         var novoTexto = 'Que bom '+nome+', espero que seu dia continue bem!<br><br> =D';
         para.innerHTML = novoTexto;
-    } else {
+    } if else {
            var novoTexto = 'Que pena '+nome+', espero que seu dia melhore! <br><br> ;-;';
            para.innerHTML = novoTexto;
      }
-    if(tudoBem=='null'){
+    else (tudoBem=='null'){
            var novoTexto = 'Que pena que você não quer se identificar, espero que na próxima você sinta-se mais avontade para dizer seu nome. <br><br> U+1F600';
            para.innerHTML = novoTexto;   
 }
