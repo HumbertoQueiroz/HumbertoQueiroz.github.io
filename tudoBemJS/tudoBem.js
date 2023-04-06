b0b2d05ee006e5eb977109af80fa804165ee6a20
Humberto Queiroz <90359980+HumbertoQueiroz@users.noreply.github.com>
2023-04-06 19:47:43 -0400

    Atualizar o tudoBem.js

--- tudoBemJS/tudoBem.js
+++ tudoBemJS/tudoBem.js
@@ -4,17 +4,19 @@ function alerta() {
     alert('Olá! Sou o  desenvolvedor Humberto Queiroz. Que bom ver você aqui!');
     var nome = prompt('Qual o seu nome?');
     var tudoBem = confirm("Tudo bem com você?");
-    if (tudoBem) {
-        var novoTexto = 'Que bom '+nome+', espero que seu dia continue bem!<br><br> =D';
-        para.innerHTML = novoTexto;
-    } else if (tudoBem==false){
-           var novoTexto = 'Que pena '+nome+', espero que seu dia melhore! <br><br> ;-;';
-           para.innerHTML = novoTexto;
-     }
+    if (nome=='null') {
+        var novoTexto = 'Que pena que você não quer se identificar, espero que na próxima você sinta-se mais avontade para dizer seu nome. <br><br> U+1F600';
+           para.innerHTML = novoTexto; 
+        }
     else {
-           var novoTexto = 'Que pena que você não quer se identificar, espero que na próxima você sinta-se mais avontade para dizer seu nome. <br><br> U+1F600';
-           para.innerHTML = novoTexto;   
-}
+            if(tudoBem) {
+                var novoTexto = 'Que bom '+nome+', espero que seu dia continue bem!<br><br> =D';
+                para.innerHTML = novoTexto;
+            } else {
+                   var novoTexto = 'Que pena '+nome+', espero que seu dia melhore! <br><br> ;-;';
+                   para.innerHTML = novoTexto;
+     }
+    }
 }
 
 
