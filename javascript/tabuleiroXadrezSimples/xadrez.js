cffa87fd93acb9672e692961cd83d5991ac9aa01
Humberto Queiroz <humbertojuridico2016@gmail.com>
2023-04-08 18:13:04 -0400

    add

---
+++ javascript/tabuleiroXadrezSimples/xadrez.js
@@ -0,0 +1,24 @@
+var para = document.querySelector('.p1');
+var para2 = document.querySelector('.p2');
+var num='';
+var x=1;
+function alerta() {
+    alert('Olá! Vamos criar um tabuleiro de xadrez?');
+    para.innerHTML='';
+    for (l=1;l<=8;l++) {
+        for(c=1;c<=8;c++){
+            p = l+c;
+            if(p%2==0){
+                num+='#';
+            }
+            else{
+                num+=' ';
+            }
+        }
+        num+='\n';        
+        
+    }
+    console.log(num);
+}
+
+
