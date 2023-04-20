cc862660a7022146ac081afe86403e0696aa229a
Humberto Queiroz <90359980+HumbertoQueiroz@users.noreply.github.com>
2023-04-20 15:55:43 -0400

    acessando json e exibindo os dados dele

--- FrontEndMentor/FrontEndMentor-1/MeuProjeto/main.js
+++ FrontEndMentor/FrontEndMentor-1/MeuProjeto/main.js
@@ -1,10 +1,43 @@
-let dadosJson = document.querySelector("#dados");
+let divDados = document.querySelector("#dados");
+let opacidade = document.querySelector(".opacidade")
+
+let divNome1 = document.querySelector("#nome1");
+let divNumero1 = document.querySelector("#numero1");
+
+let divNome2 = document.querySelector("#nome2");
+let divNumero2 = document.querySelector("#numero2");
+
+let divNome3 = document.querySelector("#nome3");
+let divNumero3 = document.querySelector("#numero3");
+
+let divNome4 = document.querySelector("#nome4");
+let divNumero4 = document.querySelector("#numero4");
+
 fetch("data.json").then((response) => {
     response.json().then((dados) => {
-        dados.usuarios.map((usuario)=> {
+        
+        divNome1.innerHTML += dados.usuarios[0].category;
+        divNumero1.innerHTML = dados.usuarios[0].score;
 
-           // dadosJson.innerHTML += '<div> ${usuario.category} - ${usuario.score} </div>'
-        })
-      
+        divNome2.innerHTML += dados.usuarios[1].category;
+        divNumero2.innerHTML = dados.usuarios[1].score + divNumero2.textContent;
+
+        divNome3.innerHTML += dados.usuarios[2].category;
+        divNumero3.innerHTML = dados.usuarios[2].score + divNumero3.textContent;
+       
+        divNome4.innerHTML += dados.usuarios[3].category;
+        divNumero4.innerHTML = dados.usuarios[3].score + divNumero4.textContent;      
     })
 })
+
+function alerta() {
+    divDados.style.display = 'inline';
+    opacidade.style.opacity = '60%';
+    opacidade.style.curso = 'hover'
+
+}
+
+function fecha(){
+    divDados.style.display = 'none';
+     opacidade.style.opacity = '';
+}
