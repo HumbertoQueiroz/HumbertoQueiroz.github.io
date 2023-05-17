568599ee35ed4bba6454d9edcf6936278863844e
Humberto Queiroz <90359980+HumbertoQueiroz@users.noreply.github.com>
2023-05-17 11:36:04 -0400

    add

--- javascript/portifolio.js
+++ javascript/portifolio.js
@@ -5,10 +5,12 @@ var descricaoHTML = document.getElementById('descricao__html');
 var descricaoCSS = document.getElementById('descricao__css');
 var descricaoJS = document.getElementById('descricao__javascript');
 var descricaoBootstraps = document.getElementById('descricao__bootstraps');
+var descricaoGithub = document.getElementById('descricao__github');
 var buttonHTML = document.getElementById('button__html');
 var buttonCss = document.getElementById('button__css');
 var buttonJs = document.getElementById('button__js');
 var buttonBootstraps = document.getElementById('button__bootstraps');
+var buttonGithub = document.getElementById('button__github');
 
 function exibeDescricao(e) {
     switch (e) {
@@ -17,6 +19,7 @@ function exibeDescricao(e) {
             descricaoCSS.style.display = 'none';
             descricaoJS.style.display = 'none';
             descricaoBootstraps.style.display = 'none';
+            descricaoGithub.style.display = 'none';
             buttonHTML.style.color = '#fff';
             buttonHTML.style.fontWeight = '900';
             buttonHTML.style.textDecoration = 'underline';
@@ -29,12 +32,16 @@ function exibeDescricao(e) {
             buttonBootstraps.style.color = '';
             buttonBootstraps.style.fontWeight = '';
             buttonBootstraps.style.textDecoration = '';
+            buttonGithub.style.color = '';
+            buttonGithub.style.fontWeight = '';
+            buttonGithub.style.textDecoration = '';
             break;
         case 'css':
             descricaoHTML.style.display = 'none';
             descricaoCSS.style.display = 'inline';
             descricaoJS.style.display = 'none';
             descricaoBootstraps.style.display = 'none';
+            descricaoGithub.style.display = 'none';
             buttonHTML.style.color = '';
             buttonHTML.style.fontWeight = '';
             buttonHTML.style.textDecoration = '';
@@ -47,12 +54,16 @@ function exibeDescricao(e) {
             buttonBootstraps.style.color = '';
             buttonBootstraps.style.fontWeight = '';
             buttonBootstraps.style.textDecoration = '';
+            buttonGithub.style.color = '';
+            buttonGithub.style.fontWeight = '';
+            buttonGithub.style.textDecoration = '';
             break;
         case 'js':
             descricaoHTML.style.display = 'none';
             descricaoCSS.style.display = 'none';
             descricaoJS.style.display = 'inline';
             descricaoBootstraps.style.display = 'none';
+            descricaoGithub.style.display = 'none';
             buttonHTML.style.color = '';
             buttonHTML.style.fontWeight = '';
             buttonHTML.style.textDecoration = '';
@@ -65,12 +76,16 @@ function exibeDescricao(e) {
             buttonBootstraps.style.color = '';
             buttonBootstraps.style.fontWeight = '';
             buttonBootstraps.style.textDecoration = '';
+            buttonGithub.style.color = '';
+            buttonGithub.style.fontWeight = '';
+            buttonGithub.style.textDecoration = '';
             break;
         case 'bootstraps':
             descricaoHTML.style.display = 'none';
             descricaoCSS.style.display = 'none';
             descricaoJS.style.display = 'none';
             descricaoBootstraps.style.display = 'inline';
+            descricaoGithub.style.display = 'none';
             buttonHTML.style.color = '';
             buttonHTML.style.fontWeight = '';
             buttonHTML.style.textDecoration = '';
@@ -83,6 +98,31 @@ function exibeDescricao(e) {
             buttonBootstraps.style.color = '#fff';
             buttonBootstraps.style.fontWeight = '900';
             buttonBootstraps.style.textDecoration = 'underline';
+            buttonGithub.style.color = '';
+            buttonGithub.style.fontWeight = '';
+            buttonGithub.style.textDecoration = '';
+            break;
+        case 'github':
+            descricaoHTML.style.display = 'none';
+            descricaoCSS.style.display = 'none';
+            descricaoJS.style.display = 'none';
+            descricaoBootstraps.style.display = 'none';
+            descricaoGithub.style.display = 'inline';
+            buttonHTML.style.color = '';
+            buttonHTML.style.fontWeight = '';
+            buttonHTML.style.textDecoration = '';
+            buttonCss.style.color = '';
+            buttonCss.style.fontWeight = '';
+            buttonCss.style.textDecoration = '';
+            buttonJs.style.color = '';
+            buttonJs.style.fontWeight = '';
+            buttonJs.style.textDecoration = '';
+            buttonBootstraps.style.color = '';
+            buttonBootstraps.style.fontWeight = '';
+            buttonBootstraps.style.textDecoration = '';
+            buttonGithub.style.color = '#fff';
+            buttonGithub.style.fontWeight = '900';
+            buttonGithub.style.textDecoration = 'underline';
             break;
     }
 }
