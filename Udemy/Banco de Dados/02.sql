3f46502ed92e309e81ab169bae7cb0cca1b04346
Humberto Queiroz <90359980+HumbertoQueiroz@users.noreply.github.com>
2023-10-25 10:48:11 -0400

    Update 02.sql

--- Udemy/Banco de Dados/02.sql
+++ Udemy/Banco de Dados/02.sql
@@ -1,4 +1,8 @@
 create schema Udemy;
+
+/* unsigned e para aceitar apenas numeros positivos, bloqueando incersao de numeros negativos
+unique key e para os dados em uma coluna serem unicos, mesmo nao sendo uma primary key */
+
 create table estados (
     id int unsigned not null auto_increment,
     nome varchar(45) not null,
@@ -10,6 +14,10 @@ create table estados (
     unique key(sigla)
 );
 
+/* Comando insert into para inserir informacao dentro de uma tabela
+podendo entre () o nome das colunas que vai ser inserido, deixando inserir menos argumentos e em outra ordem
+ou omitir e ter que informar todos os campo e na ordem certa
+Values logo em seguida para informar os valores que serao inseridos  */
 
 insert into estados (nome, sigla, regiao, populacao)
 values ('acre', 'AC', 'norte', 1.83);
@@ -19,10 +27,33 @@ insert into estados
 values ('Alagoas', 'AL', 'nordeste', 3.86),
 		('Campo Grande', 'MG', 'centro-oeste', 5),
         ('Porto Seguro', 'BH', 'sul', 1.56),
-		('Cuiabá', 'MT', 'centro-oeste', 3.54),
-        ('teste', 'ES', 'centro-oeste');
+		('Cuiabá', 'MT', 'centro-oeste', 3.54);
 
+/* select * from nomeDaTabela para exibir toda a tabela 
+pode exibir apenas algumas colunas ao trocar o * pelo nome das colunas que quer exibir 
+pode usar o as para mudar o nome da coluna que vai exibir */
 
 select * from estados;
-
 select nome, sigla from estados;
+select Sigla, nome as 'Nome do Estado' from estados;
+
+/* Filtrar os dados com where*/
+
+select Sigla, nome as 'Nome do Estado' from estados
+where regiao = 'centro-oeste';
+
+select Sigla, nome as 'Nome do Estado', populacao as 'População em milhões' from estados
+where populacao >= 3;
+
+
+/* Ordenar os resultados 
+order by para ordem crescente
+order by + desc no final para ordem decrescente*/
+
+select Sigla, nome as 'Nome do Estado', populacao as 'População em milhões' from estados
+where populacao >= 3
+order by populacao;   
+
+select Sigla, nome as 'Nome do Estado', populacao as 'População em milhões' from estados
+where populacao >= 3
+order by populacao desc;
