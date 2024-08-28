849a9c3a1b24e8d49049d1b3e70c664df3e8e4c2
Humberto Queiroz <90359980+HumbertoQueiroz@users.noreply.github.com>
2024-08-27 22:06:31 -0400

    add

---
+++ REACT/Curriculo/src/app.tsx
@@ -0,0 +1,24 @@
+
+export function App() {
+  
+
+  return (
+    <div className="w-full flex justify-center">
+      <div className="w-[1044px]  border flex justify-between">
+        <div className="w-1/4 bg-blue-900 flex flex-col items-center">
+          <img src="fotoDevHumbertoQueiroz.JPG" alt="Foto do desenvolvedor Humberto Queiroz"  className="rounded-full w-3/4 my-4"/>
+        </div>
+        <div className="w-3/4">
+          <header className="flex flex-col items-center">
+            <h1 className="mt-24 text-blue-800 text-6xl font-bold m">HUMBERTO QUEIROZ</h1>
+            <div className="border-b-4 border-slate-300 w-10/12"></div>
+            <h2>Desenvolvedor JS, TS, React.Js</h2>
+          </header>
+        </div>
+      </div>
+    </div>
+    
+  )
+    
+}
+
--- REACT/Curriculo/tailwind.config.js
+++ REACT/Curriculo/tailwind.config.js
@@ -7,7 +7,7 @@ export default {
   theme: {
     extend: {
       fontFamily:{
-        sans: ['Inter','sans-serif']
+        sans: ['Roboto','sans-serif']
       }
     },
   },
