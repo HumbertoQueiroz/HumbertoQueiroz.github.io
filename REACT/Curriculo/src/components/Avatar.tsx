f75eb9970004756496d29d078fd0be637628da22
humberto.queiroz <humberto.queiroz@cooperfibra.com.br>
2024-09-06 16:43:45 -0400

    Add - Adicionado descrição dos componentes formato JSDoc

--- REACT/Curriculo/src/components/Avatar.tsx
+++ REACT/Curriculo/src/components/Avatar.tsx
@@ -4,10 +4,18 @@ interface AvatarProps extends ImgHTMLAttributes<HTMLImageElement>{
   urlImg:string
 }
 
+/**
+ * Esse **Componente** cria uma imagem de avatar
+ * 
+ * Recebe a URL da imagem e pode receber demais propriedades de elementos
+ * @param {string} urlImg *Type: string.* - Url da imagem a ser usada como avatar
+ * @returns 
+ */
+
 export function Avatar({urlImg, ...props}:AvatarProps){
 return(
 <div className="w-full bg-blue-900 flex flex-col items-center">
-  <img src={urlImg} {...props} className="rounded-full w-3/4 my-4 hover:ring hover:ring-sky-300 hover:animate-pulse" title={props.alt}/>
+  <img src={urlImg} {...props} className="rounded-full w-3/4 my-4 ring-2 ring-sky-300" title={props.alt}/>
 </div>
 )
 }
--- REACT/Curriculo/src/components/Skill.tsx
+++ REACT/Curriculo/src/components/Skill.tsx
@@ -3,15 +3,26 @@ import {ImgHTMLAttributes } from "react";
 interface SkillInterface extends ImgHTMLAttributes <HTMLImageElement> {
   hasComponent:boolean
   icon?:React.ReactNode;
-  img?:string
+  urlImg?:string
   size?:string
   text:string
   color?:string
 }
-export function Skill ({hasComponent,icon, img, size, text, color,...props}:SkillInterface){
+
+/**
+ * Esse **Componente** cria um 'elemento' com ícone e nome da skill (conhecimento), com background pré-definido.
+ * @param {boolean} hasComponent *Type: Boolean* - Verifica se receberá um componente (**true**) como imagem ou se é uma url de imagem (**false**).
+ * @param {React.ReactNode} icon *Type: Component.* - Componente que será usado como ícone, não deve ser informado caso vá usar url de imagem, propriedade **urlImg**, passar componente com tamanho *"size"*.
+ * @param {string} urlImg *Type: string.* - URL da imagem que será usado como ícone, não deve ser informado caso vá usar componente, propriedade **icon**.
+ * @param {string} size *Type: string.* - Tamanho que será renderizado a imagem do propriedade **urlImg**, passar **propriedade do Tailwind**, usado dentro da className.
+ * @param {string} text *Type: string.* - Texto que será exibido.
+ * @param {string} color *Type: string.* - Cor que será usada no ícone do componente passado no propriedade **icon**
+ * @returns 
+ */
+export function Skill ({hasComponent,icon, urlImg, size, text, color,...props}:SkillInterface){
   if(hasComponent){
     return(
-      <div className="flex items-center gap-1 border rounded px-1 bg-slate-200 hover:scale-110 text-sm">
+      <div className="flex items-center gap-1 border rounded px-1 bg-slate-200 hover:scale-110 text-sm shadow hover:shadow-md hover:shadow-sky-200">
         <div className={color} {...props}>
         {icon}
         </div>
@@ -20,8 +31,8 @@ export function Skill ({hasComponent,icon, img, size, text, color,...props}:Skil
     )
   } else{
     return(
-      <div className="flex items-center gap-1 border rounded px-1 bg-slate-200 hover:scale-110 text-sm">
-        <img src={img} alt="" className={`rounded-full ${size}`} />
+      <div className="flex items-center gap-1 border rounded px-1 bg-slate-200 hover:scale-110 text-sm shadow hover:shadow-md hover:shadow-sky-200">
+        <img src={urlImg} alt="" className={` rounded-sm ${size}`} />
         <h3>{text}</h3>
       </div>
     )
